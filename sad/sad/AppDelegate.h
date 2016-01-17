dd6d0bcd842a2ab0d19d0ea361eac220d466641c
xiebing <931647669@qq.com>
2016-01-17 16:31:26 +0800

    init

---
+++ sad/sad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sad
+//
+//  Created by 罗玺 on 16/1/17.
+//  Copyright © 2016年 罗玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sad/sad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sad
+//
+//  Created by 罗玺 on 16/1/17.
+//  Copyright © 2016年 罗玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
